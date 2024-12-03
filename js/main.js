65e92a94bd2e8f11bc801050d3f109f7d1b425f8
DonPihot <al-husa@yandex.ru>
2024-12-03 22:11:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Пример того, как складывается строчный тип данных в js:
+
+let a = 'Hello ';
+
+let b = 'World!';
+
+let с 
+
+c = a + b;
+
+console.log(c)
+
+/* А дальше примеры правильных и неправильных названий переменных: */
+
+let tex1 = "Неправильно: "
+
+console.log(tex1, "1text, 123, @!#")
+
+let text2 = "Правильно: "
+
+console.log(text2, "text123, abcd, likeaBosS")
